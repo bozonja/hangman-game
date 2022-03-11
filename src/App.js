032d5d4480b224f0eab52aa84c707f49ae56befe
Josip Markovic <bozadesign@gmail.com>
2022-03-11 18:08:46 +0100

    Move useEffect

--- src/App.js
+++ src/App.js
@@ -17,29 +17,6 @@ function App() {
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
 
-  useEffect(() => {
-    const handleKeydown = (event) => {
-      const { key, keyCode } = event;
-      if (playable && keyCode >= 65 && keyCode <= 90) {
-        const letter = key.toLowerCase();
-        if (selectedWord.includes(letter)) {
-          if (!correctLetters.includes(letter)) {
-            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
-          } else {
-          }
-        } else {
-          if (!wrongLetters.includes(letter)) {
-            setWrongLetters((currentLetters) => [...currentLetters, letter]);
-          } else {
-          }
-        }
-      }
-    };
-    window.addEventListener("keydown", handleKeydown);
-
-    return () => window.removeEventListener("keydown", handleKeydown);
-  }, [correctLetters, wrongLetters, playable]);
-
   return (
     <>
       <BrowserRouter>
@@ -60,6 +37,9 @@ function App() {
                   selectedWord={selectedWord}
                   correctLetters={correctLetters}
                   wrongLetters={wrongLetters}
+                  playable={playable}
+                  setCorrectLetters={setCorrectLetters}
+                  setWrongLetters={setWrongLetters}
                 />
               }
             />
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,40 @@
+import { useEffect } from "react";
+
 //components
 import { Illustration } from "./Illustration";
 import Word from "./Word";
 import { WrongLetters } from "./WrongLetters";
 
-export const Game = ({ selectedWord, correctLetters, wrongLetters }) => {
+export const Game = ({
+  selectedWord,
+  correctLetters,
+  playable,
+  wrongLetters,
+  setCorrectLetters,
+  setWrongLetters,
+}) => {
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
   return (
     <>
       <div className="game-container">
